181c8909026f04b373aafa8ce2489b3a70868b56
不可预知^才精彩 <897708734@qq.com>
2016-12-26 18:14:21 +0800

    123

---
+++ TableView+xib替代scrollviewDemo/TableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  TableViewCell.h
+//  TableView+xib替代scrollviewDemo
+//
+//  Created by li on 16/12/26.
+//  Copyright © 2016年 li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIButton *voiceBtn;
+@property (weak, nonatomic) IBOutlet UIButton *presentBtn;
+@end
---
+++ TableView+xib替代scrollviewDemo/TableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.m
+//  TableView+xib替代scrollviewDemo
+//
+//  Created by li on 16/12/26.
+//  Copyright © 2016年 li. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
